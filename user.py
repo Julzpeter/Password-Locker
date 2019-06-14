95a9bc5469255da687f6fade0a05f9ed206e1172
Juliet <chepngetichjuliet@gmail.com>
2019-06-14 15:47:59 +0300

    imports credentials and adds test_init

--- user.py
+++ user.py
@@ -3,6 +3,9 @@ class User:
     """
     Class that generates new instances of users
     """
+
+    user_list = []
+    
     def _init_(self,user_name,password):
         """
         _init_method that helps us define properties for our objects
--- user_test.py
+++ user_test.py
@@ -1,5 +1,7 @@
 import unittest
 from user import User
+from credentials import Credentials
+
 class TestUser(unittest.TestCase):
     """
     Test class that defines test cases for the user class behaviours
@@ -13,3 +15,10 @@ class TestUser(unittest.TestCase):
         Set up method to run efore each test cases
         """
         self.new_user = User("JulietKoech","12338")
+        self.new_credentials = Credentials("Twitter", "Julz", "12338")
+
+    def test_init(self):
+        """test_init test case to test if the object is initialized properly
+        """
+
+
