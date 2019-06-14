d44de9839df4418b236696475a9477b55f458a24
Juliet <chepngetichjuliet@gmail.com>
2019-06-14 14:41:13 +0300

    adds an instance of the object

--- user.py
+++ user.py
@@ -4,6 +4,11 @@ class User:
     Class that generates new instances of users
     """
     def _init_(self,user_name,password):
+        """
+        _init_method that helps us define properties for our objects
+        """
+        self.user_name = user_name
+        self.password
         
 
 
