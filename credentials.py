ce17d5b0e74e9a327e696575d88459f20e3310c3
Juliet <chepngetichjuliet@gmail.com>
2019-06-14 15:12:19 +0300

    adds an instance of the object

---
+++ credentials.py
@@ -0,0 +1,14 @@
+class Credentials:
+    """
+    Class that generates new instances of contacts
+    """
+    def _init_(self,site_name,site_username,site_password):
+        """
+        _init_method that helps us define properties for our objects
+        """
+        self.site_name = site_name
+        self.site_username = site_username
+        self.site_password = self_password
+
+
+
