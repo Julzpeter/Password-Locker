7703451b5a80691f2199c462a7916a65d7cd6f22
Juliet <chepngetichjuliet@gmail.com>
2019-06-14 14:57:47 +0300

    adds a setup method and docstrings to the user_test.py

--- user_test.py
+++ user_test.py
@@ -1,1 +1,15 @@
 import unittest
+from user import User
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for the user class behaviours
+    """
+    """
+    unittest.TestCase: TestCase class that helps in creating test cases
+    """
+
+    def setUp(self):
+        """
+        Set up method to run efore each test cases
+        """
+        self.new_user = User("JulietKoech","12338")
